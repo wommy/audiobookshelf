cff86b523699f1653dda3c5d03c09508403d0399
wommy <tom@technomad.media>
2024-10-19 21:36:40 -0400

    add: nuxt-bridge

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,11 +1,12 @@
-const pkg = require('./package.json')
+import { defineNuxtConfig } from '@nuxt/bridge'
+import { version } from './package.json'
 
 const routerBasePath = process.env.ROUTER_BASE_PATH || ''
 const serverHostUrl = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3333'
 const serverPaths = ['api/', 'public/', 'hls/', 'auth/', 'feed/', 'status', 'login', 'logout', 'init']
 const proxy = Object.fromEntries(serverPaths.map((path) => [`${routerBasePath}/${path}`, { target: process.env.NODE_ENV !== 'production' ? serverHostUrl : '/' }]))
 
-module.exports = {
+export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   target: 'static',
@@ -17,7 +18,7 @@ module.exports = {
   telemetry: false,
 
   publicRuntimeConfig: {
-    version: pkg.version,
+    version,
     routerBasePath
   },
 
@@ -133,6 +134,8 @@ module.exports = {
     host: '0.0.0.0'
   },
 
+  bridge: false,
+
   /**
    * Temporary workaround for @nuxt-community/tailwindcss-module.
    *
@@ -142,4 +145,4 @@ module.exports = {
   devServerHandlers: [],
 
   ignore: ['**/*.test.*', '**/*.cy.*']
-}
+})
